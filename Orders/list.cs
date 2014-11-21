522b5446c51361c677fccf783c530518eb0897a9
Cristian C. Gorrin <cristian_gorrin@live.com>
2014-11-22 00:38:53 +0100

    Add Orders lists class

---
+++ Orders/list.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using API;
+
+namespace Orders
+{
+    public class CompanyOrders : API.Lists<CompanyOrder>
+    {
+        public CompanyOrders()
+            :base()
+        {
+        }
+    }
+
+    public class PrivetOrders : API.Lists<PrivetOrder>
+    {
+        public PrivetOrders()
+            :base()
+        {
+        }
+    }
+}
