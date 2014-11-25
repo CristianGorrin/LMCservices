a1341f3190ce629c06aba0e78a4a16206368ea44
Cristian C. Gorrin <cristian_gorrin@live.com>
2014-11-25 11:04:09 +0100

    Add Unit test for PostNo from namespace PostNo

--- UnitTestProject/TestPostNo.cs
+++ UnitTestProject/TestPostNo.cs
@@ -2,6 +2,7 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 using PostNo;
+using Interface;
 
 namespace UnitTestProject
 {
@@ -9,33 +10,22 @@ namespace UnitTestProject
     public class TestPostNo
     {
         [TestMethod]
-        public void PostNumber()
+        public void PostNo()
         {
-            PostNo.PostNo postNumber = new TestingPostNo.PostNumber();
- 
+            var postNo = new PostNo.PostNo("testCity", 1, 6300);
+
             // testing get
-            Assert.AreEqual("testCity", postNumber.City);
-            Assert.AreEqual(1, postNumber.Id);
-            Assert.AreEqual(6300, postNumber.PostNumber);
+            Assert.AreEqual("testCity", postNo.City);
+            Assert.AreEqual(1, postNo.Id);
+            Assert.AreEqual(6300, postNo.PostNumber);
 
             // testing set
-            postNumber.City = "newTestCity";
-            // TODO fix
-            string test = (string)new PrivateObject(postNumber).GetField("city");
-            Assert.AreEqual("newTestCity", test);
-
-            postNumber.PostNumber = 5000;
-        }
-    }
-
-    public class TestingPostNo 
-    {
-        public class PostNumber : PostNo.PostNo
-        {
-            public PostNumber()
-                : base("testCity", 1, 6300)
-            {
-            }
+            postNo.City = "newTestCity";
+            var city = (string)new PrivateObject(postNo).GetField("city");
+            Assert.AreEqual("newTestCity", city);
+            postNo.PostNumber = 5000;
+            var postNumber = (int)new PrivateObject(postNo).GetField("postNo");
+            Assert.AreEqual(5000, postNumber);
         }
     }
 }
