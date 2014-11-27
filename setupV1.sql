999a57f707ce5d373676d25b0265aa2cec6446dd
Cristian C. Gorrin <cristian_gorrin@live.com>
2014-11-27 20:16:41 +0100

    typo in steupV1.sql alePhoneNo, now altPhoneNo

--- setupV1.sql
+++ setupV1.sql
@@ -768,7 +768,7 @@ as
 	declare @result nvarchar(MAX)
 
 	select @result = coalesce('[tblPrivateCustomers] { privateCustomersNo = ' + CAST(privateCustomersNo as nvarchar(10)) + ', name = ' + name +
-	', surname = ' + surname + ', phoneNo = ' + phoneNo + ', alePhoneNo = ' + altPhoneNo + ', homeAddress = ' + homeAddress + 
+	', surname = ' + surname + ', phoneNo = ' + phoneNo + ', altPhoneNo = ' + altPhoneNo + ', homeAddress = ' + homeAddress + 
 	', postNo = ' + CAST(postNo as nvarchar(10)) + ', email = ' + email + ', active = ' + CAST(active as nvarchar(1)),'')
 	from [dbo].[tblPrivateCustomers] where privateCustomersNo = @privetCustomersNo
 
