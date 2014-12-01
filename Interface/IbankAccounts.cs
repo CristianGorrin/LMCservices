b84b6aa40034bc0af0fec917bb11688fd8448b8e
Cristian C. Gorrin <cristian_gorrin@live.com>
2014-12-01 11:23:32 +0100

    Add interface for bank acc.

---
+++ Interface/IbankAccounts.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interface
+{
+    public interface IbankAccounts
+    {
+        int Id { get; }
+	    string Bank { get; }
+	    string AccountName { get; }
+	    int RegNo { get; }
+	    string AccountNo { get; }
+	    double Balance { get; }
+    }
+}
