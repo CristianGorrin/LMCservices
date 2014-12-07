48b01e9a12fc11ee86fd648df405deba4343bfbf
Cristian C. Gorrin <cristian_gorrin@live.com>
2014-12-07 12:52:18 +0100

    Invoices order is now in  an array

--- Interface/IinvoiceCompany.cs
+++ Interface/IinvoiceCompany.cs
@@ -10,25 +10,6 @@ namespace Interface
     {
         int Id { get; }
         bool Active { get; }
-        Interface.IcompanyOrder? OrderNr1 { get; }
-        Interface.IcompanyOrder? OrderNr2 { get; }
-        Interface.IcompanyOrder? OrderNr3 { get; }
-        Interface.IcompanyOrder? OrderNr4 { get; }
-        Interface.IcompanyOrder? OrderNr5 { get; }
-        Interface.IcompanyOrder? OrderNr6 { get; }
-        Interface.IcompanyOrder? OrderNr7 { get; }
-        Interface.IcompanyOrder? OrderNr8 { get; }
-        Interface.IcompanyOrder? OrderNr9 { get; }
-        Interface.IcompanyOrder? OrderNr10 { get; }
-        Interface.IcompanyOrder? OrderNr11 { get; }
-        Interface.IcompanyOrder? OrderNr12 { get; }
-        Interface.IcompanyOrder? OrderNr13 { get; }
-        Interface.IcompanyOrder? OrderNr14 { get; }
-        Interface.IcompanyOrder? OrderNr15 { get; }
-        Interface.IcompanyOrder? OrderNr16 { get; }
-        Interface.IcompanyOrder? OrderNr17 { get; }
-        Interface.IcompanyOrder? OrderNr18 { get; }
-        Interface.IcompanyOrder? OrderNr19 { get; }
-        Interface.IcompanyOrder? OrderNr20 { get; }
+        Interface.IinvoiceCompany[] Order { get; }
     }
 }
--- Interface/IinvoicePrivet.cs
+++ Interface/IinvoicePrivet.cs
@@ -10,25 +10,6 @@ namespace Interface
     {
         int Id { get; }
         bool Active { get; }
-        Interface.IprivetOrder? OrderNr1 { get; }
-        Interface.IprivetOrder? OrderNr2 { get; }
-        Interface.IprivetOrder? OrderNr3 { get; }
-        Interface.IprivetOrder? OrderNr4 { get; }
-        Interface.IprivetOrder? OrderNr5 { get; }
-        Interface.IprivetOrder? OrderNr6 { get; }
-        Interface.IprivetOrder? OrderNr7 { get; }
-        Interface.IprivetOrder? OrderNr8 { get; }
-        Interface.IprivetOrder? OrderNr9 { get; }
-        Interface.IprivetOrder? OrderNr10 { get; }
-        Interface.IprivetOrder? OrderNr11 { get; }
-        Interface.IprivetOrder? OrderNr12 { get; }
-        Interface.IprivetOrder? OrderNr13 { get; }
-        Interface.IprivetOrder? OrderNr14 { get; }
-        Interface.IprivetOrder? OrderNr15 { get; }
-        Interface.IprivetOrder? OrderNr16 { get; }
-        Interface.IprivetOrder? OrderNr17 { get; }
-        Interface.IprivetOrder? OrderNr18 { get; }
-        Interface.IprivetOrder? OrderNr19 { get; }
-        Interface.IprivetOrder? OrderNr20 { get; }
+        Interface.IprivetOrder[] Order { get; }
     }
 }
