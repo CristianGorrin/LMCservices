989b64342ccda36cce7f7a7cb61d076a80a54744
Cristian C. Gorrin <cristian_gorrin@live.com>
2014-12-18 16:44:12 +0100

    Update assemblyinfo

--- LMC_GUI2/Properties/AssemblyInfo.cs
+++ LMC_GUI2/Properties/AssemblyInfo.cs
@@ -7,11 +7,11 @@ using System.Windows;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("LMC_GUI2")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("LMCServices")]
+[assembly: AssemblyDescription("ERP System")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("LMC_GUI2")]
+[assembly: AssemblyCompany("LMC Services")]
+[assembly: AssemblyProduct("LMC_GUI")]
 [assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
